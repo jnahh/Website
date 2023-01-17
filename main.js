300f4ab851982fea4dd5d0962f47585438775cc0
jnahh <118667588+jnahh@users.noreply.github.com>
2023-01-17 19:04:20 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 function reply_click(clicked_id) {
     if(clicked_id == "t1-more") {
-        document.getElementById(clicked_id).scrollIntoView();
+        document.getElementById('sec-two').scrollIntoView({
+            behavior: 'smooth'
+        });
     }
 }
