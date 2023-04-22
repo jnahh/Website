228113ff02958197cfe175be0cd0ba1a56c780d6
jnahh <118667588+jnahh@users.noreply.github.com>
2023-04-22 14:26:04 +0200

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-function reply_click(clicked_id) {
-    if(clicked_id == "t1-more") {
-        document.getElementById('sec-two').scrollIntoView({
-            behavior: 'smooth'
-        });
-    }
-}
